e0d5581387fe00cbc3b69e27265757c9578bd5f5
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2024-04-29 17:15:06 -0500

    correccion de errores cloud

--- src/main/java/apifestivos/apifestivos/controladores/FestivoControlador.java
+++ src/main/java/apifestivos/apifestivos/controladores/FestivoControlador.java
@@ -2,11 +2,9 @@ package apifestivos.apifestivos.controladores;
 
 import apifestivos.apifestivos.entidades.dtos.FestivoDto;
 import apifestivos.apifestivos.servicios.FestivoServicio;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -14,63 +12,37 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/festivos")
-// @CrossOrigin(origins = "http://localhost:4200") // Cambia esta URL según tu
-// configuración de Angular
+
 @CrossOrigin(origins = "*")
 public class FestivoControlador {
 
-    @Autowired
     private FestivoServicio festivoServicio;
 
-    @GetMapping("/verificar/{año}/{mes}/{dia}")
-    public ResponseEntity<Map<String, String>> verificarFestivo(@PathVariable int año, @PathVariable int mes,
+    public FestivoControlador(FestivoServicio festivoServicio) {
+        this.festivoServicio = festivoServicio;
+    }
+
+    @GetMapping("/verificar/{age}/{mes}/{dia}")
+    public ResponseEntity<Map<String, String>> verificarFestivo(@PathVariable int age, @PathVariable int mes,
             @PathVariable int dia) {
+        final String m = "mensaje";
         Map<String, String> respuesta = new HashMap<>();
-        if (festivoServicio.esFechaValida(String.format("%d-%02d-%02d", año, mes, dia))) {
-            if (festivoServicio.esFestivo(new Date(año - 1900, mes - 1, dia))) {
-                respuesta.put("mensaje", "Es Festivo");
+        if (festivoServicio.esFechaValida(String.format("%d-%02d-%02d", age, mes, dia))) {
+
+            if (festivoServicio.esFestivo(new Date(age - 1900, mes - 1, dia))) {
+                respuesta.put(m, "Es Festivo");
             } else {
-                respuesta.put("mensaje", "No es festivo");
+                respuesta.put(m, "No es festivo");
             }
         } else {
-            respuesta.put("mensaje", "Fecha no válida");
+            respuesta.put(m, "Fecha no válida");
         }
         return ResponseEntity.ok(respuesta);
     }
 
-    /*
-     * @GetMapping("/verificar/{año}/{mes}/{dia}")
-     * public String verificarFestivo(@PathVariable int año, @PathVariable int mes,
-     * 
-     * @PathVariable int dia) {
-     * String respuesta;
-     * if (festivoServicio.esFechaValida(String.format("%d-%02d-%02d", año, mes,
-     * dia))) {
-     * if (festivoServicio.esFestivo(new Date(año - 1900, mes - 1, dia))) {
-     * respuesta = "Es Festivo";
-     * } else {
-     * respuesta = "No es festivo";
-     * }
-     * return respuesta;//new ResponseEntity<>(respuesta, HttpStatus.OK);
-     * } else {
-     * return "Fecha no válida";//new ResponseEntity<>("Fecha no válida",
-     * HttpStatus.BAD_REQUEST);
-     * }
-     * }
-     */
-
-    /*
-     * @GetMapping("/obtener")
-     * public ResponseEntity<List<FestivoDto>> obtenerFestivos(@RequestParam int
-     * año) {
-     * List<FestivoDto> festivos = festivoServicio.obtenerFestivos(año);
-     * return new ResponseEntity<>(festivos, HttpStatus.OK);
-     * }
-     */
-
-    @RequestMapping(value = "/obtener/{año}", method = RequestMethod.GET)
-    public ResponseEntity<List<FestivoDto>> obtener(@PathVariable int año) {
-        List<FestivoDto> festivos = festivoServicio.obtenerFestivos(año);
+    @RequestMapping(value = "/obtener/{age}", method = RequestMethod.GET)
+    public ResponseEntity<List<FestivoDto>> obtener(@PathVariable int age) {
+        List<FestivoDto> festivos = festivoServicio.obtenerFestivos(age);
 
         if (festivos != null && !festivos.isEmpty()) {
             return new ResponseEntity<>(festivos, HttpStatus.OK);
@@ -78,34 +50,4 @@ public class FestivoControlador {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-    /*
-     * 
-     * @RequestMapping(value = "/obtener/{año}", method = RequestMethod.GET)
-     * public List<FestivoDto> obtener(@PathVariable int año) {
-     * FestivoServicio servicio;
-     * return festivoServicio.obtenerFestivos(año);
-     * }
-     */
-
-    /*
-     * @PostMapping("/guardar")
-     * public ResponseEntity<Void> guardarFestivo(@RequestBody FestivoDto
-     * festivoDto) {
-     * festivoServicio.guardarFestivo(festivoDto);
-     * return new ResponseEntity<>(HttpStatus.CREATED);
-     * }
-     * 
-     * @PutMapping("/actualizar/{id}")
-     * public ResponseEntity<Void> actualizarFestivo(@PathVariable Long
-     * id, @RequestBody FestivoDto festivoDto) {
-     * festivoServicio.actualizarFestivo(id, festivoDto);
-     * return new ResponseEntity<>(HttpStatus.OK);
-     * }
-     * 
-     * @DeleteMapping("/eliminar/{id}")
-     * public ResponseEntity<Void> eliminarFestivo(@PathVariable Long id) {
-     * festivoServicio.eliminarFestivo(id);
-     * return new ResponseEntity<>(HttpStatus.OK);
-     * }
-     */
 }
